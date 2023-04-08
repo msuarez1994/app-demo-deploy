7868942a206b6425ebd15fd1196b1dbda2a52a73
Mauricio Suarez <msuarezm1994@gmail.com>
2023-04-08 15:17:41 -0600

    se agrego ruta de health

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const port = 8000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/health', (req, res) => {
+  res.send('Ok!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
